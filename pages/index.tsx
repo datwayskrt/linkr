83e45df44e46eaecde0d214deb18ad09abcaf45a
whoisxnio <enioblvck@gmail.com>
2022-11-25 04:41:11 +0100

    uplaoding all so far

---
+++ pages/index.tsx
@@ -0,0 +1,49 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>linkr</title>
+        <meta name="description" content="all your links in one place" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+
+        <div className={styles.menu}>
+        <h1>
+          link<a href="#">r.</a>
+        </h1>
+        <div>
+        <a href='#'>join waiting list</a>
+        <button>
+          =
+        </button>
+        </div>
+      </div> 
+
+        <h1 className={styles.title}>
+          All Your Links<br/>
+          In One Place.
+        </h1>
+        <p className={styles.description}>
+          A whole new way of connecting with your fans!
+        </p>
+
+        <div className={styles.grid}>
+          <img src="/iphone.png" alt="iPhone Demo"/>
+        </div>
+
+        <form className={styles.form}>
+          <input type="email" placeholder='Enter your email'/>
+          <input type="submit" value="join waiting list"/>
+        </form>
+      </main>
+
+      
+    </div>
+  )
+}
